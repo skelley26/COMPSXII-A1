38ba796f4c1128fd96bb16b8a9e1a163e489fa71
Sam <kellesam20@gmail.com>
2026-09-13 15:10:35 -0500

    completed memory detective assignment

--- memory_detective.c
+++ memory_detective.c
@@ -24,6 +24,10 @@ void investigate_data_types() {
     // - double called 'my_double' with value 2.71828
     // - char called 'my_char' with value 'C'
     
+    int my_int = 42;
+    float my_float = 3.14f;
+    double my_double = 2.71828;
+    char my_char = 'C';
     
     
     
@@ -38,8 +42,15 @@ void investigate_data_types() {
     //        my_int, (void*)&my_int, sizeof(my_int));
     
     
-    
-    
+    printf("int: value = %d, address = %p, size = %zu bytes\n", 
+           my_int, (void*)&my_int, sizeof(my_int));
+    printf("float: value = %f, address = %p, size = %zu bytes\n", 
+           my_float, (void*)&my_float, sizeof(my_float));
+    printf("double: value = %lf, address = %p, size = %zu bytes\n", 
+           my_double, (void*)&my_double, sizeof(my_double));
+    printf("char: value = %c, address = %p, size = %zu bytes\n", 
+           my_char, (void*)&my_char, sizeof(my_char));
+
 }
 
 // =============================================================================
@@ -54,25 +65,28 @@ void investigate_stack() {
     // - A float called 'stack_float' with value 3.14
     // - A char called 'stack_char' with value 'A'
     
-    
-    
-    
+    int stack_int = 42;
+    float stack_float = 3.14f;
+    char stack_char = 'A';
+
     printf("\n=== STACK ALLOCATION ===\n");
     // TODO: Print the address and value of each stack variable
     // Use format specifier %p for addresses and appropriate specifiers for values
     // Example: printf("stack_int: address = %p, value = %d\n", (void*)&stack_int, stack_int);
-    
-    
-    
+    printf("stack_int: address = %p, value = %d\n", (void*)&stack_int, stack_int);
+    printf("stack_float: address = %p, value = %f\n", (void*)&stack_float, stack_float);
+    printf("stack_char: address = %p, value = %c\n", (void*)&stack_char, stack_char);
+
 }
 
 void nested_function_call() {
     // TODO: Declare a local integer called 'nested_var' with value 999
-    
+    int nested_var = 999;
     
     printf("\n=== NESTED FUNCTION STACK ===\n");
     // TODO: Print the address and value of nested_var
     // Observe: Is this address higher or lower than variables in investigate_stack()?
+    printf("nested_var: address = %p, value = %d\n", (void*)&nested_var, nested_var);
     
 }
 
@@ -84,9 +98,10 @@ void nested_function_call() {
 // and have fixed memory addresses.
 
 // TODO: Declare a global integer variable called 'global_counter' and initialize it to 100
-
+int global_counter = 100;
 
 // TODO: Declare a static integer variable called 'static_value' and initialize it to 200
+static int static_value = 200;
 
 
 void investigate_data_segment() {
@@ -95,8 +110,9 @@ void investigate_data_segment() {
     // TODO: Print the addresses and values of global_counter and static_value
     // Example: printf("global_counter: address = %p, value = %d\n", 
     //                 (void*)&global_counter, global_counter);
-    
-    
+    printf("global_counter: address = %p, value = %d\n", (void*)&global_counter, global_counter);
+    printf("static_value: address = %p, value = %d\n", (void*)&static_value, static_value);
+
 }
 
 
@@ -112,7 +128,7 @@ void investigate_heap() {
     // TODO: Use malloc() to allocate memory for an integer on the heap
     // Store the returned pointer in a variable called 'heap_int'
     // Hint: int *heap_int = (int*)malloc(sizeof(int));
-    
+    void *heap_int = malloc(sizeof(int));
     
     // TODO: Check if malloc succeeded (heap_int should not be NULL)
     // If successful:
@@ -122,7 +138,11 @@ void investigate_heap() {
     //
     // Example: printf("heap_int: address = %p, value = %d, size = %zu bytes\n",
     //                 (void*)heap_int, *heap_int, sizeof(int));
-    
+heap_int = malloc(sizeof(int));
+    if (heap_int != NULL) {
+        *(int*)heap_int = 777;
+printf("heap_int: address = %p, value = %d, size = %zu bytes\n",
+           (void*)heap_int, *(int*)heap_int, sizeof(int));
     
     
     
@@ -130,14 +150,14 @@ void investigate_heap() {
     // TODO: Free the allocated memory using free(heap_int)
     // Note: After free(), the pointer still holds the address, but the memory
     // is no longer yours to use (we'll explore this more in Unit 3)
-    
+    free(heap_int);
 }
 
 // =============================================================================
 // MAIN FUNCTION
 // =============================================================================
 
-int main() {
+int main(); {
     printf("=============================================================\n");
     printf("         MEMORY DETECTIVE: Investigating Program Memory\n");
     printf("=============================================================\n");
